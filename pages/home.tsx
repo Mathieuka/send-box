159c19a8da4b3edd52ca9f73a3624f544ff403b5
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2021-04-11 12:49:13 +0200

    home update

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,11 +1,13 @@
-import { useQuery, useMutation } from '@apollo/client';
-import { Button, Input } from '@material-ui/core';
 import { useState } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
+import { Button, Input, Drawer } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 import { GET_ALL_USERS } from '../src/graphql/query';
 import { ADD_USER } from '../src/graphql/mutation';
 
 const Home = (): JSX.Element => {
     const [username, setUsername] = useState('');
+    const [drawer, setDrawer] = useState(false);
     const { loading, error, data, refetch } = useQuery(GET_ALL_USERS);
     const [addUser] = useMutation(ADD_USER);
 
@@ -17,10 +19,24 @@ const Home = (): JSX.Element => {
         return <div>Loading ...</div>;
     }
 
+    const toggleDrawer = (open: boolean) => (
+        event: KeyboardEvent | MouseEvent
+    ) => {
+        if (
+            event.type === 'keydown' &&
+            (((event as unknown) as React.KeyboardEvent).key === 'Tab' ||
+                ((event as unknown) as React.KeyboardEvent).key === 'Shift')
+        ) {
+            return;
+        }
+
+        setDrawer(false);
+    };
+
     const usersList = data.users.map((user: any) => (
         <div
             key={user.id}
-            className="border-2 w-96 p-10 rounded-2xl border-blue-500 my-3"
+            className="border-2 p-5 rounded-2xl border-blue-500 my-3"
         >
             <div>Name: {user.name}</div>
             <div>Id: {user.id}</div>
@@ -28,28 +44,36 @@ const Home = (): JSX.Element => {
     ));
 
     return (
-        <div className="h-screen grid place-content-center">
-            <div className="max-h-96">
-                <form
-                    className="grid gap-10"
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        addUser({
-                            variables: { name: username },
-                        });
-                        setUsername('');
-                        refetch();
-                    }}
-                >
-                    <Input
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <Button type="submit" variant="contained">
-                        Add user
-                    </Button>
-                </form>
-                <div className="grid justify-center overflow-scroll">
+        <div>
+            <div className="grid place-content-end">
+                <MenuIcon onClick={() => setDrawer(true)} />
+            </div>
+            <Drawer anchor="left" open={drawer} onClose={toggleDrawer(false)}>
+                <div>Content</div>
+            </Drawer>
+            <div className="h-screen grid place-content-center mx-20 gap-5">
+                <div className="max-h-64">
+                    <form
+                        className="grid gap-10"
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            addUser({
+                                variables: { name: username },
+                            });
+                            setUsername('');
+                            refetch();
+                        }}
+                    >
+                        <Input
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <Button type="submit" variant="contained">
+                            Add user
+                        </Button>
+                    </form>
+                </div>
+                <div className=" p-5 max-h-64 grid justify-center overflow-scroll">
                     {usersList}
                 </div>
             </div>
