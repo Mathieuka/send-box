4ee28bf67404fc2222b6e522d37a4187e9cca326
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2021-12-06 11:29:31 +0100

    binomial algorithm worked

--- src/algorithmic/colorChoice.ts
+++ src/algorithmic/colorChoice.ts
@@ -1,40 +1,31 @@
 const buildMatrix = (posters: number, colorsAvailable: number) => {
-    let matrix: number[][] = [
-        [1, ...(Array.from({ length: colorsAvailable }).fill(0) as number[])],
-    ];
-    for (let idx = 1; idx <= colorsAvailable; idx += 1) {
+    let matrix: number[][] = [];
+    for (let idx = 0; idx <= colorsAvailable; idx += 1) {
         matrix = [
-            ...matrix,
-            [
-                1,
-                ...(Array.from({ length: colorsAvailable }).fill(
-                    0
-                ) as number[]),
-            ],
+            ...(matrix.length ? matrix : ([] as number[][])),
+            [1, ...Array.from({ length: colorsAvailable }).fill(0)] as number[],
         ];
     }
-    return matrix;
+    return matrix as number[][];
 };
 
 export const checkChoose = (posters: number, colorsAvailable: number): any => {
     if (posters === colorsAvailable) return 1;
     const matrix = buildMatrix(posters, colorsAvailable);
-    for (let matrixIdx = 0; matrixIdx < matrix.length; matrixIdx += 1) {
+    for (let rowIdx = 0; rowIdx < matrix.length; rowIdx += 1) {
         for (
-            let vectorIdx = 0;
-            vectorIdx < matrix[matrixIdx].length;
-            vectorIdx += 1
+            let columnIdx = 0;
+            columnIdx < matrix[rowIdx].length;
+            columnIdx += 1
         ) {
-            if (matrixIdx !== 0 && vectorIdx <= matrixIdx) {
-                if (matrix[matrixIdx][vectorIdx] === 0) {
-                    matrix[matrixIdx][vectorIdx] =
-                        matrix[matrixIdx - 1][vectorIdx] +
-                        matrix[matrixIdx - 1][vectorIdx - 1];
-                    if (
-                        matrixIdx <= colorsAvailable &&
-                        matrix[matrixIdx][vectorIdx] === posters
-                    ) {
-                        return vectorIdx;
+            if (rowIdx && columnIdx <= rowIdx) {
+                if (!matrix[rowIdx][columnIdx]) {
+                    const previousRaw = matrix[rowIdx - 1];
+                    const currentRaw = matrix[rowIdx];
+                    currentRaw[columnIdx] =
+                        previousRaw[columnIdx] + previousRaw[columnIdx - 1];
+                    if (matrix[rowIdx][columnIdx] === posters) {
+                        return columnIdx;
                     }
                 }
             }
