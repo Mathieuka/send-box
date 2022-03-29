18e9d37c440a36cadf1ebed434bdf0fe5a445dae
MK <mathieu.kahlaoui.pro@gmail.com>
2022-03-29 16:13:13 +0200

    test refactoring

--- src/algorithmic/__tests__/superStreetFigterSelection.test.ts
+++ src/algorithmic/__tests__/superStreetFigterSelection.test.ts
@@ -2,7 +2,7 @@ import { superStreetFighterSelection } from '../superStreetFigtherSelection';
 
 describe('superStreetFighterSelection', () => {
     describe('Moving right', () => {
-        test('Basic without blank', () => {
+        test('Without blank case', () => {
             const fighters = [
                 ['', 'Ryu', 'E.Honda', 'Blanka', 'Guile', ''],
                 ['Balrog', 'Ken', 'Chun Li', 'Zangief', 'Dhalsim', 'Sagat'],
@@ -35,7 +35,7 @@ describe('superStreetFighterSelection', () => {
             ]);
         });
 
-        test('With blank', () => {
+        test('With blank case', () => {
             const fighters = [
                 ['', 'E.Honda', 'Blanka', 'Guile', 'Ryu', ''],
                 ['Balrog', 'Ken', 'Chun Li', 'Zangief', 'Dhalsim', 'Sagat'],
@@ -51,7 +51,7 @@ describe('superStreetFighterSelection', () => {
         });
     });
     describe('Moving Left', () => {
-        test('Basic without blank', () => {
+        test('Without blank case', () => {
             const fighters = [
                 ['', 'Ryu', 'E.Honda', 'Blanka', 'Guile', ''],
                 ['Balrog', 'Ken', 'Chun Li', 'Zangief', 'Dhalsim', 'Sagat'],
@@ -84,7 +84,7 @@ describe('superStreetFighterSelection', () => {
             ]);
         });
 
-        test('Basic with blank', () => {
+        test('With blank case', () => {
             const fighters = [
                 ['', 'Ryu', 'E.Honda', 'Blanka', 'Guile', ''],
                 ['Balrog', 'Ken', 'Chun Li', 'Zangief', 'Dhalsim', 'Sagat'],
@@ -116,17 +116,6 @@ describe('superStreetFighterSelection', () => {
                 'E.Honda',
             ]);
         });
-
-        test('With blank', () => {
-            const fighters = [
-                ['', '', 'Ryu', 'E.Honda', 'Blanka', 'Guile', '', ''],
-                ['Balrog', 'Ken', 'Chun Li', 'Zangief', 'Dhalsim', 'Sagat'],
-                ['Vega', 'T.Hawk', 'Fei Long', 'Deejay', 'Cammy', 'M.Bison'],
-            ];
-            expect(
-                superStreetFighterSelection(fighters, [0, 6], ['left', 'left'])
-            ).toEqual(['Guile', 'Blanka']);
-        });
     });
 
     describe('Moving Vertically', () => {
@@ -148,136 +137,136 @@ describe('superStreetFighterSelection', () => {
         });
     });
 
-    test('Longer Grid 1', () => {
-        const fighters4 = [
-            ['', 'Ryu', 'E.Honda', 'Cammy'],
-            ['Balrog', 'Ken', 'Chun Li', ''],
-            ['Vega', '', 'Fei Long', 'Balrog'],
-            ['Blanka', 'Guile', '', 'Chun Li'],
-            ['M.Bison', 'Zangief', 'Dhalsim', 'Sagat'],
-            ['Deejay', 'Cammy', '', 'T.Hawk'],
-        ];
-        const position = [3, 3];
-        const moves = [
-            'left',
-            'left',
-            'down',
-            'right',
-            'right',
-            'right',
-            'right',
-            'down',
-            'left',
-            'left',
-            'left',
-            'left',
-            'up',
-            'right',
-            'right',
-            'up',
-            'right',
-            'right',
-            'right',
-        ];
-        const solution = [
-            'Guile',
-            'Blanka',
-            'M.Bison',
-            'Zangief',
-            'Dhalsim',
-            'Sagat',
-            'M.Bison',
-            'Deejay',
-            'T.Hawk',
-            'Cammy',
-            'Deejay',
-            'T.Hawk',
-            'Sagat',
-            'M.Bison',
-            'Zangief',
-            'Guile',
-            'Chun Li',
-            'Blanka',
-            'Guile',
-        ];
-        expect(superStreetFighterSelection(fighters4, position, moves)).toEqual(
-            solution
-        );
+    describe('Moving random', () => {
+        test('Random move 1', () => {
+            const fighters4 = [
+                ['', 'Ryu', 'E.Honda', 'Cammy'],
+                ['Balrog', 'Ken', 'Chun Li', ''],
+                ['Vega', '', 'Fei Long', 'Balrog'],
+                ['Blanka', 'Guile', '', 'Chun Li'],
+                ['M.Bison', 'Zangief', 'Dhalsim', 'Sagat'],
+                ['Deejay', 'Cammy', '', 'T.Hawk'],
+            ];
+            const position = [3, 3];
+            const moves = [
+                'left',
+                'left',
+                'down',
+                'right',
+                'right',
+                'right',
+                'right',
+                'down',
+                'left',
+                'left',
+                'left',
+                'left',
+                'up',
+                'right',
+                'right',
+                'up',
+                'right',
+                'right',
+                'right',
+            ];
+            const solution = [
+                'Guile',
+                'Blanka',
+                'M.Bison',
+                'Zangief',
+                'Dhalsim',
+                'Sagat',
+                'M.Bison',
+                'Deejay',
+                'T.Hawk',
+                'Cammy',
+                'Deejay',
+                'T.Hawk',
+                'Sagat',
+                'M.Bison',
+                'Zangief',
+                'Guile',
+                'Chun Li',
+                'Blanka',
+                'Guile',
+            ];
+            expect(
+                superStreetFighterSelection(fighters4, position, moves)
+            ).toEqual(solution);
+        });
+
+        test('Random move 2', () => {
+            const fighters4 = [
+                ['', 'Ryu', 'E.Honda', 'Cammy'],
+                ['Balrog', 'Ken', 'Chun Li', ''],
+                ['Vega', '', 'Fei Long', 'Balrog'],
+                ['Blanka', 'Guile', '', 'Chun Li'],
+                ['M.Bison', 'Zangief', 'Dhalsim', 'Sagat'],
+                ['Deejay', 'Cammy', '', 'T.Hawk'],
+            ];
+            const position = [0, 3];
+            const moves = [
+                'left',
+                'left',
+                'down',
+                'right',
+                'right',
+                'right',
+                'right',
+                'down',
+                'left',
+                'left',
+                'left',
+                'left',
+                'down',
+                'right',
+                'right',
+                'down',
+                'right',
+                'right',
+                'right',
+                'down',
+                'left',
+                'left',
+                'left',
+                'down',
+                'left',
+                'left',
+                'left',
+            ];
+
+            const solution = [
+                'E.Honda',
+                'Ryu',
+                'Ken',
+                'Chun Li',
+                'Balrog',
+                'Ken',
+                'Chun Li',
+                'Fei Long',
+                'Vega',
+                'Balrog',
+                'Fei Long',
+                'Vega',
+                'Blanka',
+                'Guile',
+                'Chun Li',
+                'Sagat',
+                'M.Bison',
+                'Zangief',
+                'Dhalsim',
+                'Dhalsim',
+                'Zangief',
+                'M.Bison',
+                'Sagat',
+                'T.Hawk',
+                'Cammy',
+                'Deejay',
+                'T.Hawk',
+            ];
+            expect(
+                superStreetFighterSelection(fighters4, position, moves)
+            ).toEqual(solution);
+        });
     });
-
-    test('Longer Grid 2', () => {
-        const fighters4 = [
-            ['', 'Ryu', 'E.Honda', 'Cammy'],
-            ['Balrog', 'Ken', 'Chun Li', ''],
-            ['Vega', '', 'Fei Long', 'Balrog'],
-            ['Blanka', 'Guile', '', 'Chun Li'],
-            ['M.Bison', 'Zangief', 'Dhalsim', 'Sagat'],
-            ['Deejay', 'Cammy', '', 'T.Hawk'],
-        ];
-        const position = [0, 3];
-        const moves = [
-            'left',
-            'left',
-            'down',
-            'right',
-            'right',
-            'right',
-            'right',
-            'down',
-            'left',
-            'left',
-            'left',
-            'left',
-            'down',
-            'right',
-            'right',
-            'down',
-            'right',
-            'right',
-            'right',
-            'down',
-            'left',
-            'left',
-            'left',
-            'down',
-            'left',
-            'left',
-            'left',
-        ];
-
-        const solution = [
-            'E.Honda',
-            'Ryu',
-            'Ken',
-            'Chun Li',
-            'Balrog',
-            'Ken',
-            'Chun Li',
-            'Fei Long',
-            'Vega',
-            'Balrog',
-            'Fei Long',
-            'Vega',
-            'Blanka',
-            'Guile',
-            'Chun Li',
-            'Sagat',
-            'M.Bison',
-            'Zangief',
-            'Dhalsim',
-            'Dhalsim',
-            'Zangief',
-            'M.Bison',
-            'Sagat',
-            'T.Hawk',
-            'Cammy',
-            'Deejay',
-            'T.Hawk',
-        ];
-        expect(superStreetFighterSelection(fighters4, position, moves)).toEqual(
-            solution
-        );
-    });
-
-    // test('Random values', () => {});
 });
--- src/algorithmic/superStreetFigtherSelection.ts
+++ src/algorithmic/superStreetFigtherSelection.ts
@@ -1,4 +1,5 @@
 // www.codewars.com/kata/58583922c1d5b415b00000ff/train/typescript
+
 export function superStreetFighterSelection(
     fighters: Array<string[]>,
     position: number[],
@@ -10,62 +11,65 @@ export function superStreetFighterSelection(
     let currentRow: number = position[0];
     let currentColumn: number = position[1];
 
+    const escapeTheSlotWhileIsEmpty = (direction: string) => {
+        while (fighters[currentRow][currentColumn] === '') {
+            currentColumn =
+                direction === 'right' ? currentColumn + 1 : currentColumn - 1;
+        }
+    };
+
     moves.forEach((direction: string) => {
-        if (direction === 'right') {
-            if (
-                fighters[currentRow][currentColumn + 1] === '' ||
-                currentColumn + 1 > fighters[0].length - 1
-            ) {
-                if (currentColumn + 1 >= fighters[0].length - 1) {
-                    currentColumn = 0;
-                } else {
-                    currentColumn += 1;
+        const nextCaseIsOutsideTheFrame =
+            direction === 'right'
+                ? currentColumn + 1 > colLength
+                : currentColumn - 1 < 0;
+        const nextSlotIsEmpty =
+            direction === 'right'
+                ? fighters[currentRow][currentColumn + 1] === ''
+                : fighters[currentRow][currentColumn - 1] === '';
+        const canGoUp =
+            direction === 'up' &&
+            currentRow > 0 &&
+            fighters[currentRow - 1][currentColumn] !== '';
+        const canGoDown =
+            direction === 'down' &&
+            currentRow < rowLength &&
+            fighters[currentRow + 1][currentColumn] !== '';
+
+        if (direction === 'right' || direction === 'left') {
+            if (nextSlotIsEmpty || nextCaseIsOutsideTheFrame) {
+                if (direction === 'right') {
+                    if (currentColumn + 1 >= colLength) {
+                        currentColumn = 0;
+                    } else {
+                        currentColumn += 1;
+                    }
                 }
-                // shift on right as long as on empty case
-                while (fighters[currentRow][currentColumn] === '') {
-                    currentColumn += 1;
+
+                if (direction === 'left') {
+                    if (currentColumn - 1 <= 0) {
+                        currentColumn = colLength;
+                    } else {
+                        currentColumn -= 1;
+                    }
                 }
+                escapeTheSlotWhileIsEmpty(direction);
             } else {
-                currentColumn += 1;
+                currentColumn += direction === 'right' ? +1 : -1;
             }
         }
 
-        if (direction === 'left') {
-            if (
-                fighters[currentRow][currentColumn - 1] === '' ||
-                currentColumn - 1 < 0
-            ) {
-                if (currentColumn - 1 <= 0) {
-                    currentColumn = colLength;
-                } else {
-                    currentColumn -= 1;
-                }
-                // shift on left as long as on empty case
-                while (fighters[currentRow][currentColumn] === '') {
-                    currentColumn -= 1;
-                }
-            } else {
-                currentColumn -= 1;
+        if (direction === 'up' || direction === 'down') {
+            if (canGoUp) {
+                currentRow -= 1;
             }
-        }
-
-        if (direction === 'up') {
-            if (currentRow > 0) {
-                if (fighters[currentRow - 1][currentColumn] !== '') {
-                    currentRow -= 1;
-                }
-            }
-        }
-
-        if (direction === 'down') {
-            if (currentRow < rowLength) {
-                if (fighters[currentRow + 1][currentColumn] !== '') {
-                    currentRow += 1;
-                }
+            if (canGoDown) {
+                currentRow += 1;
             }
         }
 
         result.push(fighters[currentRow][currentColumn]);
     });
+
     return result;
 }
