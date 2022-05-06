709ed76a3d9b847f1036679b457896069b2848df
MK <mathieu.kahlaoui.pro@gmail.com>
2022-05-06 21:58:37 +0200

    doors refactoring

--- src/algorithmic/__tests__/doors.test.ts
+++ src/algorithmic/__tests__/doors.test.ts
@@ -1,18 +1,16 @@
 import { doors } from '../doors';
 
 describe('Open doors', () => {
-    test('1', () => {
+    test('1 door', () => {
         expect(doors(1)).toEqual(1);
     });
-    test('2', () => {
+    test('2 doors', () => {
         expect(doors(2)).toEqual(1);
     });
-    test('5 ', () => {
+    test('5 doors', () => {
         expect(doors(5)).toEqual(2);
     });
-    test('52900 ', () => {
-        console.time('CONSOLE TIME');
+    test('52900 doors', () => {
         expect(doors(52900)).toEqual(230);
-        console.timeEnd('CONSOLE TIME');
     });
 });
--- src/algorithmic/doors.ts
+++ src/algorithmic/doors.ts
@@ -1,15 +1,9 @@
 export const doors = (n: number): number => {
-    const doors: boolean[] = [];
-    for (let idx = 1; idx <= n; idx += 1) {
-        doors.push(false);
-    }
-
-    for (let lap = 1; lap <= n; lap += 1) {
-        for (let i = 0; i < n; i += 1) {
-            if ((i + 1) % lap === 0) {
-                doors[i] = !doors[i];
-            }
+    const doors: boolean[] = Array.from({ length: n }, () => true);
+    for (let lap = 2; lap <= n; lap += 1) {
+        for (let idxDoor = lap - 1; idxDoor < n; idxDoor += lap) {
+            doors[idxDoor] = !doors[idxDoor];
         }
     }
-    return doors.filter((v) => v).length;
+    return doors.filter((isOpen) => isOpen).length;
 };
