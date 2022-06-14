214d0f51a20df7b0b2c7c0b7a92d31658ef0e878
MK <mathieu.kahlaoui.pro@gmail.com>
2022-06-14 14:00:02 +0200

    algo(algoExpert): stopwatch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,77 @@
 import '../styles/globals.css';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import { ApolloProvider } from '@apollo/client';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import theme from '../src/muiTheme';
-import useApollo from '../src/apollo';
+import { useRef, useState } from 'react';
 
-const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-    const apolloClient = useApollo(pageProps.initialApolloState);
+const MyApp = (): JSX.Element => {
+    const [timerText, setTimerText] = useState('00:00:000');
+    const [timerId, setTimerId] = useState<number>(0);
+
+    const millisElapsedBeforeLastStart = useRef(0);
+    const lastTimerStartTime = useRef(0);
+
+    function formatNumber(numb: number, desiredLength: number) {
+        const stringNumber = String(numb);
+        return stringNumber.padStart(desiredLength, '0');
+    }
+
+    function updateTimer() {
+        const millisElapsed =
+            Date.now() -
+            lastTimerStartTime.current +
+            millisElapsedBeforeLastStart.current;
+        const secondsElapsed = millisElapsed / 1000;
+        const minutesElapsed = secondsElapsed / 10;
+
+        const millisText = formatNumber(millisElapsed % 1000, 3);
+        const secondsText = formatNumber(Math.floor(secondsElapsed) % 60, 2);
+        const minutesText = formatNumber(Math.floor(minutesElapsed), 2);
+
+        setTimerText(`${minutesText}:${secondsText}:${millisText}`);
+        const id = requestAnimationFrame(updateTimer);
+        setTimerId(id);
+    }
+
+    function startTimer() {
+        lastTimerStartTime.current = Date.now();
+        const id = requestAnimationFrame(updateTimer);
+        setTimerId(id);
+    }
+
+    function stopTimer() {
+        millisElapsedBeforeLastStart.current =
+            millisElapsedBeforeLastStart.current +
+            Date.now() -
+            lastTimerStartTime.current;
+        cancelAnimationFrame(timerId);
+    }
+
+    function resetTimer() {
+        setTimerText('00:00:000');
+        millisElapsedBeforeLastStart.current = 0;
+    }
 
     return (
-        <ApolloProvider client={apolloClient}>
-            <Head>
-                <title>Back-Office</title>
-            </Head>
-            <MuiThemeProvider theme={theme}>
-                <Component {...pageProps} apolloClient={apolloClient} />
-            </MuiThemeProvider>
-        </ApolloProvider>
+        <>
+            <div id="stopwatch">
+                <div id="timer" role="timer">
+                    {timerText}
+                </div>
+                <div>
+                    <button type="button" onClick={startTimer}>
+                        START
+                    </button>
+                </div>
+                <div>
+                    <button type="button" onClick={stopTimer}>
+                        STOP
+                    </button>
+                </div>
+                <div>
+                    <button type="button" onClick={resetTimer}>
+                        RESET
+                    </button>
+                </div>
+            </div>
+        </>
     );
 };
 
