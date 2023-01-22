6b4192ecfef30c9c872af8bed7bb4fe54d40b5d4
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2023-01-22 16:26:06 -0500

    algo(Spiral traverse): Is working, should be refactored

--- src/algorithmic/__tests__/spiralTraverse.test.ts
+++ src/algorithmic/__tests__/spiralTraverse.test.ts
@@ -10,22 +10,147 @@ describe('spiralTraverse', () => {
         expect(spiralTraverse(input)).toEqual([1, 2, 3, 4]);
     });
 
-    test('(wip) should return the correct output 3x3 matrix', () => {
-        const input = [
-            [1, 2, 3],
-            [8, 9, 4],
-            [7, 6, 5],
-        ];
-
-        expect(spiralTraverse(input)).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
-    });
-
-    test('(final) should return the correct output 3x3 matrix', () => {
+    test('should return the correct output', () => {
         const input = [
             [1, 2, 3],
             [8, 9, 4],
             [7, 6, 5],
         ];
         expect(spiralTraverse(input)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    });
+
+    test('should return the correct output', () => {
+        const input = [
+            [1, 2, 3, 4],
+            [12, 13, 14, 5],
+            [11, 16, 15, 6],
+            [10, 9, 8, 7],
+        ];
+        expect(spiralTraverse(input)).toEqual([
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16,
+        ]);
+    });
+
+    test('More rows then columns', () => {
+        const input = [
+            [4, 2, 3, 6, 7, 8, 1, 9, 5, 10],
+            [12, 19, 15, 16, 20, 18, 13, 17, 11, 14],
+        ];
+        expect(spiralTraverse(input)).toEqual([
+            4,
+            2,
+            3,
+            6,
+            7,
+            8,
+            1,
+            9,
+            5,
+            10,
+            14,
+            11,
+            17,
+            13,
+            18,
+            20,
+            16,
+            15,
+            19,
+            12,
+        ]);
+    });
+
+    test('At least same quantities of rows as columns', () => {
+        const input = [
+            [27, 12, 35, 26],
+            [25, 21, 94, 11],
+            [19, 96, 43, 56],
+            [55, 36, 10, 18],
+            [96, 83, 31, 94],
+            [93, 11, 90, 16],
+        ];
+        expect(spiralTraverse(input)).toEqual([
+            27,
+            12,
+            35,
+            26,
+            11,
+            56,
+            18,
+            94,
+            16,
+            90,
+            11,
+            93,
+            96,
+            55,
+            19,
+            25,
+            21,
+            94,
+            43,
+            10,
+            31,
+            83,
+            36,
+            96,
+        ]);
+    });
+
+    test('Simple array', () => {
+        const input = [[1, 3, 2, 5, 4, 7, 6]];
+        expect(spiralTraverse(input)).toEqual([1, 3, 2, 5, 4, 7, 6]);
+    });
+
+    test('multiple rows', () => {
+        // eslint-disable-next-line prettier/prettier
+        const input = [
+            [1],
+            [3],
+            [2],
+            [5],
+            [4],
+            [7],
+            [6]
+        ];
+        expect(spiralTraverse(input)).toEqual([1, 3, 2, 5, 4, 7, 6]);
+    });
+
+    test('More columns then rows', () => {
+        // eslint-disable-next-line prettier/prettier
+        const input = [
+            [1, 2, 3, 4],
+            [10, 11, 12, 5],
+            [9, 8, 7, 6],
+        ];
+        expect(spiralTraverse(input)).toEqual([
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+        ]);
     });
 });
--- src/algorithmic/spiralTraverse.ts
+++ src/algorithmic/spiralTraverse.ts
@@ -1,32 +1,53 @@
 export function spiralTraverse(array: number[][]) {
     const result: number[] = [];
 
-    const columnStartIdx = 0;
+    if (array.length === 1) {
+        return [...array[0]];
+    }
+
+    if (array.length <= 2) {
+        return [...array[0], ...array[1].reverse()];
+    }
+
+    let columnStartIdx = 0;
     let rowStartIdx = 0;
 
     let columnEndIdx = array[0].length;
     let rowEndIdx = array.length - 1;
 
-    for (let i = columnStartIdx; i < columnEndIdx; i += 1) {
-        result.push(array[rowStartIdx][i]);
-    }
+    // while (rowStartIdx <= rowEndIdx) {
+    while (columnStartIdx < columnEndIdx) {
+        // row in
+        for (let i = columnStartIdx; i < columnEndIdx; i += 1) {
+            result.push(array[rowStartIdx][i]);
+        }
 
-    rowStartIdx += 1;
+        rowStartIdx += 1;
 
-    for (let i = rowStartIdx; i <= rowEndIdx; i += 1) {
-        result.push(array[i][columnEndIdx - 1]);
-    }
+        // column in
+        for (let i = rowStartIdx; i <= rowEndIdx; i += 1) {
+            result.push(array[i][columnEndIdx - 1]);
+        }
 
-    columnEndIdx -= 1;
+        columnEndIdx -= 1;
 
-    for (let i = columnEndIdx - 1; i >= columnStartIdx; i -= 1) {
-        result.push(array[rowEndIdx][i]);
-    }
+        if (rowStartIdx < rowEndIdx) {
+            // row out
+            for (let i = columnEndIdx - 1; i >= columnStartIdx; i -= 1) {
+                result.push(array[rowEndIdx][i]);
+            }
 
-    rowEndIdx -= 1;
+            rowEndIdx -= 1;
+        }
 
-    for (let i = rowEndIdx; i >= rowStartIdx; i -= 1) {
-        result.push(array[i][columnStartIdx]);
+        if (columnStartIdx < columnEndIdx) {
+            // column out
+            for (let i = rowEndIdx; i >= rowStartIdx; i -= 1) {
+                result.push(array[i][columnStartIdx]);
+            }
+
+            columnStartIdx += 1;
+        }
     }
 
     return result;
