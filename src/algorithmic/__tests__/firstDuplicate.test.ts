ee2dbfc94b7f32c0cb4d39cfbf673f298aa1a0aa
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2023-10-01 17:29:30 +0200

    algo(firstDuplicateValue): is working and should be refactored and optimized certainly

--- src/algorithmic/__tests__/firstDuplicate.test.ts
+++ src/algorithmic/__tests__/firstDuplicate.test.ts
@@ -1,8 +1,30 @@
 import { firstDuplicateValue } from '../firstDuplicate';
 
 describe('Should return the first duplicate', () => {
+    test('Length 0', () => {
+        expect(firstDuplicateValue([0])).toEqual(-1);
+    });
+
+    test('Length 1', () => {
+        expect(firstDuplicateValue([1])).toEqual(-1);
+    });
+
     test('Length 2', () => {
         expect(firstDuplicateValue([1, 2])).toEqual(-1);
         expect(firstDuplicateValue([1, 1])).toEqual(1);
+    });
+
+    test.skip('Length 3', () => {
+        expect(firstDuplicateValue([1, 2, 2])).toEqual(2);
+        expect(firstDuplicateValue([1, 2, 1])).toEqual(1);
+    });
+
+    test('Length 4', () => {
+        // expect(firstDuplicateValue([3, 2, 2, 3])).toEqual(2);
+        expect(firstDuplicateValue([2, 3, 3, 2])).toEqual(3);
+    });
+
+    test('Length 6', () => {
+        expect(firstDuplicateValue([1, 1, 3, 2, 2, 3])).toEqual(1);
     });
 });
--- src/algorithmic/firstDuplicate.ts
+++ src/algorithmic/firstDuplicate.ts
@@ -1,9 +1,35 @@
+const hasTwin = (currNumber: number, array: number[]) =>
+    array.findIndex((number) => number === currNumber);
+
 export function firstDuplicateValue(array: number[]) {
-    if (array.length === 2) {
+    const obj: Record<string, number> = {};
+    let result: null | number = -1;
+
+    if (array.length === 0) return -1;
+
+    if (array.length <= 2) {
         if (array[0] === array[1]) {
             return array[0];
         }
+        return -1;
     }
 
-    return -1;
+    for (let i = 0; i < array.length; i += 1) {
+        const index = hasTwin(array[i], array.slice(i + 1));
+
+        if (index !== -1) {
+            const clone: any[] = [...array];
+            clone[i] = '';
+            const ind = clone.findIndex((val) => val === array[i]);
+
+            if (!obj[array[i]]) {
+                if (result === -1 || obj[result] > ind) {
+                    result = array[i];
+                }
+                obj[array[i]] = ind;
+            }
+        }
+    }
+
+    return result;
 }
