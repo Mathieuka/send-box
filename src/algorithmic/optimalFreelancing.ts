4fc93215ca2dd793dcf09f3f233fd6eb13c15792
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2024-01-29 21:02:04 +0100

    algo(optimalFreelancing): refactoring in wip

--- src/algorithmic/optimalFreelancing.ts
+++ src/algorithmic/optimalFreelancing.ts
@@ -1,52 +1,51 @@
 type Jobs = { deadline: number; payment: number }[];
 
 export function optimalFreelancing(jobs: Jobs) {
+    let count = 0;
     const days = Array.from({ length: 7 }).fill(false) as boolean[];
     let total = 0;
     const jobsByPayment = jobs.sort((a, b) => b.payment - a.payment);
 
-    for (let i = 0; i < 7; i += 1) {
+    for (let i = 0; i < jobsByPayment.length; i += 1) {
+        count += 1;
+        const slotIsEmpty = !days[jobsByPayment[i]?.deadline - 1];
+
         // we set the slot to true with the best payment is the slot is free
-        if (
-            !days[jobsByPayment[i]?.deadline - 1] &&
-            jobsByPayment[i]?.payment
-        ) {
+        if (slotIsEmpty) {
             if (jobsByPayment[i].deadline < 8) {
                 total += jobsByPayment[i].payment;
                 days[jobsByPayment[i].deadline - 1] = true;
             }
-        } else {
-            // if the current slot is already uses, we check iteratively the previous slot
-            let index = jobsByPayment[i]?.deadline - 1 || 0;
+        }
 
-            const j = jobsByPayment[i];
+        if (!slotIsEmpty) {
+            // if the current slot is already uses, we check iteratively the previous slot
+            let index = jobsByPayment[i]?.deadline - 1;
 
             while (index >= 0) {
-                if (!days[index] && j?.payment) {
-                    total += j.payment;
+                count += 1;
+                if (!days[index]) {
+                    total += jobsByPayment[i].payment;
                     days[index] = true;
-                    index = -1;
+                    index = 0;
                 }
-
                 index -= 1;
             }
         }
     }
 
-    const jobWithDeadLineGreaterThan7Days = jobsByPayment
-        .filter((job) => job.deadline > 7)
-        .sort((a, b) => b.payment - a.payment);
+    const jobWithDeadLineGreaterThan7Days = jobsByPayment.filter(
+        (job) => job.deadline > 7
+    );
 
-    let index = 0;
     for (let i = 0; i < days.length; i += 1) {
-        if (!days[i]) {
-            if (jobWithDeadLineGreaterThan7Days[index]) {
-                total += jobWithDeadLineGreaterThan7Days[index].payment;
-                days[i] = true;
-                index += 1;
-            }
+        count += 1;
+        if (!days[i] && jobWithDeadLineGreaterThan7Days[0]) {
+            total += jobWithDeadLineGreaterThan7Days[0].payment;
+            jobWithDeadLineGreaterThan7Days.shift();
+            days[i] = true;
         }
     }
-
+    console.log('%c LOG count', 'background: #222; color: #bada55', count);
     return total;
 }
